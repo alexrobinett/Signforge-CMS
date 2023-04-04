d771a8ed03f3614f4af67d223dc907b3f26d72b3
robinetta <alxr19@gmail.com>
2023-04-04 02:07:35 -0400

    auth slice created

--- app/store.js
+++ app/store.js
@@ -1,10 +1,11 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
+import authReducer from '../app/features/auth/authSlice'
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
-
+        auth: authReducer
     },
     middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(apiSlice.middleware),
