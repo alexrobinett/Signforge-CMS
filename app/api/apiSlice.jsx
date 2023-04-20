fa6f6dfbd752cdcbb275f30209539bd15749272e
robinetta <alxr19@gmail.com>
2023-04-19 21:41:29 -0400

    update base url

--- app/api/apiSlice.jsx
+++ app/api/apiSlice.jsx
@@ -3,7 +3,7 @@ import { setCredentials } from '../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
 
-    baseUrl: 'http://127.0.0.1:3000/',
+    baseUrl: 'https://signage-api-production.up.railway.app',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
