7a74cce626fbc0acf9f2275c2d90c17d44ca7035
robinetta <alxr19@gmail.com>
2023-05-02 12:23:40 -0400

    dev branch

--- app/api/apiSlice.jsx
+++ app/api/apiSlice.jsx
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from '../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
+  // baseUrl: 'http://127.0.0.1:5173/' dev url
   baseUrl: 'https://signage-api-production.up.railway.app',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
